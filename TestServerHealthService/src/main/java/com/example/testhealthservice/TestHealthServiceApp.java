36d8ad85647a6f26c3a2115e6166ae18f057bdbd
unknown <naresh.duggena@gmail.com>
2020-01-25 19:31:27 +0530

    controller is added

---
+++ TestServerHealthService/src/main/java/com/example/testhealthservice/TestHealthServiceApp.java
@@ -0,0 +1,13 @@
+package com.example.testhealthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHealthServiceApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestHealthServiceApp.class, args);
+	}
+	
+}
---
+++ TestServerHealthService/src/main/java/com/example/testhealthservice/controller/TestHealthController.java
@@ -0,0 +1,17 @@
+package com.example.testhealthservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestHealthController {
+
+	@RequestMapping(method=RequestMethod.GET, value= "/apphealth")
+	public String testAppHealth() {
+		String response = "Application is up and running";
+		System.out.println(response);
+		return response;
+		
+	}
+}
